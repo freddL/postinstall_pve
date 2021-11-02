e7a860e9bbc846a63f49d05cf963d5ad712cd72b
freddL <freddL@users.noreply.github.com>
2021-11-02 11:53:36 +0100

    Update postinstall_pve7.sh

--- postinstall_pve7.sh
+++ postinstall_pve7.sh
@@ -91,7 +91,7 @@ fi
 echo -e '\033[1;33m Mise a jour du serveur Proxmox \033[0m'
 apt update && apt -y full-upgrade && apt -y dist-upgrade
 echo -e '\033[1;33m Installation des outils \033[0m'
-apt install -y  dirmngr pigz htop iptraf iotop iftop snmpd ncdu ethtool snmp-mibs-downloader apticron net-tools dnsutils ifupdown2 mlocate screen ncdu ntfs-3g --force-yes
+apt install -y  dirmngr pigz htop iptraf iotop iftop snmpd ncdu ethtool snmp-mibs-downloader apticron net-tools dnsutils ifupdown2 mlocate screen ncdu ntfs-3g ipmitool --force-yes
 apt autoremove -y
 apt clean
 
